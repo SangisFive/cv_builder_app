249733b31d59cc66796758e21bb18979e620183a
sanzhar.sangispayev <sangis960807@gmail.com>
2022-11-25 01:57:12 +0100

    init app

---
+++ cv_builder_app/src/main/java/cz/sangis/personal/cv_builder_app/CvBuilderAppApplication.java
@@ -0,0 +1,13 @@
+package cz.sangis.personal.cv_builder_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvBuilderAppApplication {
+
+    public static void main( String[] args ) {
+        SpringApplication.run( CvBuilderAppApplication.class, args );
+    }
+
+}
---
+++ cv_builder_app/src/test/java/cz/sangis/personal/cv_builder_app/CvBuilderAppApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.sangis.personal.cv_builder_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvBuilderAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
